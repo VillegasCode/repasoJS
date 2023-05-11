bcf150a0e2be34e9a66de59e461725376cc0e69f
Carlos Villegas <carlos.villegasch@gmail.com>
2023-05-10 19:37:32 -0500

    Condicional para saber si una persona es ALTA, PROMEDIO o BAJA

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ alert('Hola mundo desde el main.js');
 
 //vatiables
 var nombre = "Alex Villegas";
-var altura = 175;
+var altura = 185;
 
 var concatenacion = nombre + " " + altura;
 
@@ -11,8 +11,18 @@ var concatenacion = nombre + " " + altura;
 var datos = document.getElementById("datos");
 // datos.innerHTML = concatenacion;
 
+// Colocando texto y datos dentro de un DIV con JS y comillas simple
 datos.innerHTML = `
      <h1>Soy la caja de datos</h1>
      <h2>Mi nombre es: ${nombre}</h2>
      <h3>Mido: ${altura} cm</h3>
 `;
+
+//Condicionales
+if(altura >= 180){
+    datos.innerHTML += '<h1>Eres una persona ALTA</h1>';
+} else if (altura < 180 && altura > 165) {
+    datos.innerHTML += '<h1>Eres una persona de tamaño PROMEDIO</h1>';
+} else {
+    datos.innerHTML += '<h1>Eres una persona BAJA</h1>'
+}
