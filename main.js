b933b437342c69fa49acf60e6914c4c7f07ffe5a
Carlos Villegas <carlos.villegasch@gmail.com>
2023-05-10 19:14:43 -0500

    document.getElementByID and NombreVariable.innerHTML and lines HTML inside of quotes

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
 alert('Hola mundo desde el main.js');
+
+//vatiables
+var nombre = "Alex Villegas";
+var altura = 175;
+
+var concatenacion = nombre + " " + altura;
+
+// document.write(concatenacion);
+
+var datos = document.getElementById("datos");
+// datos.innerHTML = concatenacion;
+
+datos.innerHTML = `
+     <h1>Soy la caja de datos</h1>
+     <h2>Mi nombre es: ${nombre}</h2>
+     <h3>Mido: ${altura} cm</h3>
+`;
