85ebaa80a08fc6afd76e93a69c7c18c94a03e7c3
Carlos Villegas <carlos.villegasch@gmail.com>
2023-05-11 21:30:40 -0500

    Creating promises with 2 seconds to load the alert with response JS

--- main.js
+++ main.js
@@ -97,3 +97,23 @@ document.write("<h1> Marca: " + coche.marca + "</br> Modelo: " + coche.modelo + 
 console.log("Torque: " + coche.Torque);
 
 coche.mostrarDatos();
+
+//Crear PROMESA
+var  saludar = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        let saludo = "Hola muy buenas a todos chavales"
+        //saludo = false;
+        if (saludo){
+            resolve(saludo);
+        } else {
+            reject("No hay saludo disponible");
+        }
+    }, 2000)
+});
+
+saludar.then(resultado => {
+    alert(resultado);
+})
+.catch(err => {
+    alert(err);
+});
